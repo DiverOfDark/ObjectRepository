07deb2bcaf0d7db1db8aaf19bbcf9900282ebabe
Kirill Orlov <i@kirillorlov.pro>
2019-02-17 13:00:48 +0300

    fix hangfire expiration manager

--- src/OutCode.EscapeTeams.ObjectRepository.Hangfire/ExpirationManager.cs
+++ src/OutCode.EscapeTeams.ObjectRepository.Hangfire/ExpirationManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using Hangfire.Logging;
 using Hangfire.Server;
@@ -20,7 +21,18 @@ namespace OutCode.EscapeTeams.ObjectRepository.Hangfire
         public void Execute(CancellationToken cancellationToken)
         {
             Logger.Debug($"Removing outdated records...");
-            _storage.ObjectRepository.Remove<JobModel>(s => s.ExpireAt < DateTime.UtcNow);
+
+            var badJobs = _storage.ObjectRepository.Set<JobModel>().Where(v => v.ExpireAt < DateTime.UtcNow);
+
+            var badStates = _storage.ObjectRepository.Set<StateModel>().Where(v => badJobs.Contains(v.Job)).ToList();
+            var badParameters = _storage.ObjectRepository.Set<JobParameterModel>()
+                .Where(v => badJobs.Any(t => t.Id == v.JobId)).ToList();
+            
+            _storage.ObjectRepository.RemoveRange(badJobs);
+            _storage.ObjectRepository.RemoveRange(badStates);
+            _storage.ObjectRepository.RemoveRange(badParameters);
+            
+            _storage.ObjectRepository.Remove<CounterModel>(s=>s.ExpireAt < DateTime.UtcNow);
             _storage.ObjectRepository.Remove<ListModel>(s => s.ExpireAt < DateTime.UtcNow);
             _storage.ObjectRepository.Remove<SetModel>(s => s.ExpireAt < DateTime.UtcNow);
             _storage.ObjectRepository.Remove<HashModel>(s => s.ExpireAt < DateTime.UtcNow);
